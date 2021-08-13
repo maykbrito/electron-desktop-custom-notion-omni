92ad833728f3e18d6b4ccaa10c0afb5b5e9b2cf0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-13 10:06:10 -0300

    preload now load style.css content

--- preload.js
+++ preload.js
@@ -1,6 +1,11 @@
+const { ipcRenderer } = require('electron')
+const path = require('path')
+const fs = require('fs')
+const appPath = ipcRenderer.sendSync('request-app-path')
+const cssContent = fs.readFileSync(path.resolve(appPath , 'styles', 'style.css'))
 
 window.addEventListener("DOMContentLoaded", () => {
   const styleEl = document.createElement("style");
-  styleEl.innerHTML = customStyle;
+  styleEl.innerHTML = cssContent
   document.head.append(styleEl);
 });
