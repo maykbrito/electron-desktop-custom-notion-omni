8a532f6a97bee229d7aa5ef0aad21af7b43e8d75
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-19 21:18:58 -0300

    add createWindowControls function

--- preload.js
+++ preload.js
@@ -5,6 +5,24 @@ const appPath = ipcRenderer.sendSync('request-app-path')
 const cssContent = fs.readFileSync(path.resolve(appPath, 'styles', 'style.css'))
 
 window.addEventListener("DOMContentLoaded", () => {
+
+function createWindowControls() {
+  const iconsFolder = path.resolve(appPath, 'assets', 'windowsIcons')
+  const wrapper = document.createElement('div')
+  wrapper.id = "window-controls-wrapper"
+  
+  wrapper.innerHTML = `
+  <div class="button" id="minimize">
+    ${fs.readFileSync(path.resolve(iconsFolder, 'minimize.svg'))}
+  </div>
+  <div class="button" id="expand">
+    ${fs.readFileSync(path.resolve(iconsFolder, 'square.svg'))}
+  </div>
+  <div class="button" id="close">
+    ${fs.readFileSync(path.resolve(iconsFolder, "close.svg"))}
+  </div>`
+  return wrapper
+}
   const styleEl = document.createElement("style");
   styleEl.innerHTML = cssContent
   document.head.append(styleEl);
