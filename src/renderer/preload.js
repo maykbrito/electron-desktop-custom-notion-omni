35d1d431b03d0d493e9e61ef56b263c9ef8de77d
Mayk Brito <maykbrito@gmail.com>
2021-09-01 13:58:06 -0300

    refactor: organizing folders and files

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -3,12 +3,11 @@ const path = require('path')
 const fs = require('fs')
 const appPath = ipcRenderer.sendSync('request-app-path')
 
-
 function createWindowControls() {
   const iconsFolder = path.resolve(appPath, 'assets', 'windowsIcons')
   const wrapper = document.createElement('div')
-  wrapper.id = "window-controls-wrapper"
-  
+  wrapper.id = 'window-controls-wrapper'
+
   wrapper.innerHTML = `
   <div class="button" id="minimize">
     ${fs.readFileSync(path.resolve(iconsFolder, 'minimize.svg'))}
@@ -17,20 +16,22 @@ function createWindowControls() {
     ${fs.readFileSync(path.resolve(iconsFolder, 'square.svg'))}
   </div>
   <div class="button" id="close">
-    ${fs.readFileSync(path.resolve(iconsFolder, "close.svg"))}
+    ${fs.readFileSync(path.resolve(iconsFolder, 'close.svg'))}
   </div>`
   return wrapper
 }
 
 function injectCSS(cssPath) {
   const cssContent = fs.readFileSync(cssPath)
-  const styleEl = document.createElement("style");
+  const styleEl = document.createElement('style')
   styleEl.innerHTML = cssContent
   document.head.append(styleEl)
 }
 
-function createWindowsMenu(){
-  injectCSS(path.resolve(appPath, 'styles', 'windowControls.css'))
+function createWindowsMenu() {
+  injectCSS(
+    path.resolve(appPath, 'src', 'renderer', 'styles', 'windowControls.css')
+  )
   const windowControlsMenu = createWindowControls()
   document.body.appendChild(windowControlsMenu)
 
@@ -39,41 +40,41 @@ function createWindowsMenu(){
   window.addEventListener('mousemove', event => {
     const { pageX, pageY } = event
     const { x, height, y } = windowControlsMenu.getBoundingClientRect()
-    if(pageY <= 10 && pageX > x){
-      if(!windowControlsMenu.classList.contains('active')){
+    if (pageY <= 10 && pageX > x) {
+      if (!windowControlsMenu.classList.contains('active')) {
         windowControlsMenu.classList.add('active')
       }
     }
 
-    if(pageY > y + height + 10 || pageX < x){
-      if(windowControlsMenu.classList.contains('active')){
+    if (pageY > y + height + 10 || pageX < x) {
+      if (windowControlsMenu.classList.contains('active')) {
         hideMenu()
       }
     }
   })
 
-    const minimize_btn = document.getElementById("minimize")
-    minimize_btn.onclick = ()=> {
-      ipcRenderer.send('minimize') 
-      hideMenu()
-    }
+  const minimize_btn = document.getElementById('minimize')
+  minimize_btn.onclick = () => {
+    ipcRenderer.send('minimize')
+    hideMenu()
+  }
 
-    const maxime_btn = document.getElementById("expand")
-    maxime_btn.onclick = () => {
-      ipcRenderer.send('expand')
-      hideMenu()
-    }
+  const maxime_btn = document.getElementById('expand')
+  maxime_btn.onclick = () => {
+    ipcRenderer.send('expand')
+    hideMenu()
+  }
 
-    const close_btn = document.getElementById("close")
-    close_btn.onclick = () => {
-      ipcRenderer.send('close')
-      hideMenu()
-    }
+  const close_btn = document.getElementById('close')
+  close_btn.onclick = () => {
+    ipcRenderer.send('close')
+    hideMenu()
+  }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  injectCSS(path.resolve(appPath, 'styles', 'style.css'))
-  if(process.platform !== "darwin"){
+window.addEventListener('DOMContentLoaded', () => {
+  injectCSS(path.resolve(appPath, 'src', 'renderer', 'styles', 'style.css'))
+  if (process.platform !== 'darwin') {
     createWindowsMenu()
   }
-});
+})
