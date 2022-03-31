f50eb5909782121ac2250838f533b62daaf1787d
Mayk Brito <maykbrito@gmail.com>
2022-03-31 10:25:01 -0300

    fix: cover image as style bg

--- src/renderer/modules/slides/bg-cover.js
+++ src/renderer/modules/slides/bg-cover.js
@@ -1,9 +1,9 @@
 module.exports.backgroundCover = function () {
   let bgImage = document.querySelector(
-    '#notion-app div.notion-scroller.vertical.horizontal img'
+    '#notion-app div.notion-scroller [contenteditable="false"] img[style*="object-fit: cover"]'
   )
 
-  if (bgImage === 'undefined') return
+  if (bgImage === 'undefined' || bgImage === null) return
 
   let imgElement = document.createElement('img')
   imgElement.src = bgImage.src
