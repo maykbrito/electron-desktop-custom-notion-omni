2405d235d66dfa3eb5104443f1d1f1ef3d60e892
Mayk Brito <maykbrito@gmail.com>
2022-10-11 13:55:41 -0300

    fix: background must appear in heading 1 only

--- src/renderer/modules/slides/bg-cover.js
+++ src/renderer/modules/slides/bg-cover.js
@@ -1,13 +1,13 @@
 module.exports.backgroundCover = function () {
   let bgImage = document.querySelector(
-    '#notion-app div.notion-scroller [contenteditable="false"] img[style*="object-fit: cover"]'
+    '#notion-app div.notion-scroller [contenteditable="false"] img[style*="height: 30vh"]'
   )
 
-  if (bgImage === 'undefined' || bgImage === null) return
+  if (bgImage === "undefined" || bgImage === null) return
 
-  let imgElement = document.createElement('img')
+  let imgElement = document.createElement("img")
   imgElement.src = bgImage.src
-  imgElement.classList.add('slides__cover-image')
+  imgElement.classList.add("slides__cover-image")
 
   return imgElement
 }
