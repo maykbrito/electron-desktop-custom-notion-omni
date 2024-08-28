7e21847004be06b4c2855f2267411d8c404c8dbd
Mayk Brito <maykbrito@gmail.com>
2024-08-28 11:36:55 -0300

    update styles

--- src/renderer/utils/inject-css.js
+++ src/renderer/utils/inject-css.js
@@ -1,12 +1,12 @@
-const { ipcRenderer } = require('electron')
-const path = require('path')
-const appPath = ipcRenderer.sendSync('request-app-path')
-const fs = require('fs')
+const { ipcRenderer } = require("electron")
+const path = require("path")
+const appPath = ipcRenderer.sendSync("request-app-path")
+const fs = require("fs")
 
 function injectCSS(...cssPathSegments) {
   const cssPath = path.resolve(appPath, ...cssPathSegments)
   const cssContent = fs.readFileSync(cssPath)
-  const styleEl = document.createElement('style')
+  const styleEl = document.createElement("style")
   styleEl.innerHTML = cssContent
   document.head.append(styleEl)
 }
